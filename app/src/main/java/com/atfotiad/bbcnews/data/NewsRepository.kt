5109952e9c2da602bb6a328cffd1800896742688
atfotiad <thfotiad@gmail.com>
2024-08-28 03:56:12 +0100

    Remove activityRetainedScope from injected class due to testing errors.

--- app/src/main/java/com/atfotiad/bbcnews/data/NewsRepository.kt
+++ app/src/main/java/com/atfotiad/bbcnews/data/NewsRepository.kt
@@ -2,9 +2,8 @@ package com.atfotiad.bbcnews.data
 
 import com.atfotiad.bbcnews.BuildConfig
 import com.atfotiad.bbcnews.api.NewsClient
-import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
-@ActivityRetainedScoped
+
 class NewsRepository @Inject constructor(
     private val newsClient: NewsClient
 ) {
