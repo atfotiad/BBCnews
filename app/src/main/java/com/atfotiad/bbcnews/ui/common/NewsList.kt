be51e14fd8064d40afc85e89ecdf1b9bbc391964
atfotiad <thfotiad@gmail.com>
2024-08-29 16:39:02 +0100

    Add testTag for the list

--- app/src/main/java/com/atfotiad/bbcnews/ui/common/NewsList.kt
+++ app/src/main/java/com/atfotiad/bbcnews/ui/common/NewsList.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import com.atfotiad.bbcnews.data.model.Article
 
 @Composable
@@ -13,7 +14,7 @@ fun NewsList(
     onClick: (Article) -> Unit
 ) {
     LazyColumn(
-        modifier
+        modifier.testTag("list")
     ) {
         items(count = list.size) { index ->
             val article = list[index]
